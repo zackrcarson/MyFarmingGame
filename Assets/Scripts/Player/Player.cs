17e31f9ef589a91bf9947e07549ae851c54cac10
zackcarson <zack.carson@virginia.edu>
2020-08-22 15:53:37 -0600

    Added scenery fade out and fade in methods for when the player walks behind them

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -32,9 +32,9 @@ public class Player : SingletonMonobehaviour<Player>
     public bool idleRight;
     
     private Rigidbody2D rigidBody2D;
-
+#pragma warning disable 414
     private Direction playerDirection;
-
+#pragma warning restore 414
     private float movementSpeed;
 
     private bool _playerInputIsDisabled = false;
