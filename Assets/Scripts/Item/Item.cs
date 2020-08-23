e30420b44586f4d09cfce051fed37887e85218d2
zackcarson <zack.carson@virginia.edu>
2020-08-23 16:43:20 -0600

    Added item nudge script to nudge reapable scenary as you walk through it

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -25,7 +25,23 @@ public class Item : MonoBehaviour
     }
 
     public void Init(int itemCodeParam)
-    {
+    {   
+        // default item code is 0, so as long as it's been set up.
+        if (itemCodeParam != 0)
+        {
+            ItemCode = itemCodeParam;
 
+            // Use the singleton monobehavious inventory manager class' instance method GetItemDetails to return all of the item details for the given item code
+            ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(ItemCode);
+            
+            // Make sure the sprite is set up correctly based on the item details sprite!
+            spriteRenderer.sprite = itemDetails.itemSprite;
+
+            // If the item type is reapable (item type enum!), then add a nudgeable component
+            if (itemDetails.itemType == ItemType.Reapable_scenary)
+            {
+                gameObject.AddComponent<ItemNudge>();
+            }
+        }
     }
 }
