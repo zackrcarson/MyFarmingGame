c8744c5d8070430779444db58fd6e73b7148baec
zackrcarson <zackrcarson@gmail.com>
2020-08-22 18:01:27 -0600

    Created item details and item types enum

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -12,3 +12,18 @@ public enum Direction
     right,
     none
 }
+
+public enum ItemType
+{
+    Seed,
+    Commodity,
+    Watering_tool,
+    Hoeing_tool,
+    Chopping_tool,
+    Breaking_tool,
+    Reaping_tool,
+    Collecting_tool,
+    Reapable_scenary,
+    none,
+    count
+}
