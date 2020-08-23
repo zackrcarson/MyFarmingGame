cef04881068e7eb46e030df12da74062ad378f90
zackcarson <zack.carson@virginia.edu>
2020-08-22 21:52:31 -0600

    Added items class and items prefab

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private int _itemCode;
+
+    private SpriteRenderer spriteRenderer;
+
+    public int ItemCode { get { return _itemCode; } set { _itemCode = value;} }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        if (ItemCode != 0)
+        {
+            Init(ItemCode);
+        }
+    }
+
+    public void Init(int itemCodeParam)
+    {
+
+    }
+}
