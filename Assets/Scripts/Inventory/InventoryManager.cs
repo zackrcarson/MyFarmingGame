94e3b917e055a595111c1e52d6496d4238d19213
zackcarson <zack.carson@virginia.edu>
2020-08-22 22:24:55 -0600

    Created an inventory manager script and an item pickup script

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : SingletonMonobehaviour<InventoryManager>
+{
+    // Create a dictionary for inventory, with itemCode : itemDetails. This will be fast to access
+    private Dictionary<int, ItemDetails> itemDetailsDictionary;
+
+    [SerializeField] private SO_ItemList itemList = null;
+
+    private void Start()
+    {
+        // Create item details dictionary
+        CreateItemDetailsDictionary();
+    }
+
+    /// <summary>
+    /// Populates the itemDetailsDictionary from the scriptable object items list
+    /// </summary>
+    private void CreateItemDetailsDictionary()
+    {
+        itemDetailsDictionary = new Dictionary<int, ItemDetails>();
+
+        // Loop through each of the item details options (code, name, sprite, description, etc) in each item in the SO item list
+        foreach (ItemDetails itemDetails in itemList.itemDetails)
+        {
+            // For each item in the item list, populate the dictionary with the item code as a key, and the details as a value
+            itemDetailsDictionary.Add(itemDetails.itemCode, itemDetails);
+        }
+    }
+
+    /// <summary>
+    /// Returns the itemDetails from the SO_ItemList for the itemCode, or null if the item code doesn't exist
+    /// </summary>
+    public ItemDetails GetItemDetails(int itemCode)
+    {
+        ItemDetails itemDetails;
+
+        // Check if the given item code is in the dictionary. If so, return the details. If not, return null.
+        if (itemDetailsDictionary.TryGetValue(itemCode, out itemDetails))
+        {
+            return itemDetails;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{   
+    // Gets called by unity every time the player collides with an item with a trigger collider
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Grab the item component of the object we collided with (may be null if it's not an item!)
+        Item item = collision.GetComponent<Item>();
+
+        // Check if it was indeed an item! 
+        if (item != null)
+        {
+            // Then get it's item details from InventoryManager if it is an item
+            // InventoryManager inherits from SingletonMonobehavious, so .Instance gets the vars/methods.
+            // The GetItemDetails is a method that returns the item details object given an item code
+            ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(item.ItemCode);
+
+            // Print the item description to console
+            Debug.Log(itemDetails.itemDescription);
+        }
+
+    }
+}
