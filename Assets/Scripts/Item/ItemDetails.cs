4c48e54a79306ee863dc93fd74b1b5513517b777
zackcarson <zack.carson@virginia.edu>
2020-08-22 18:01:27 -0600

    Created item details and item types enum

---
+++ Assets/Scripts/Item/ItemDetails.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ItemDetails
+{
+    public int itemCode;
+
+    public Sprite itemSprite;
+
+    public ItemType itemType;
+    public string itemDescription;
+    public string itemLongDescription;
+    
+    public short itemUseGridRadius;
+    public float itemUseRadius;
+
+    public bool isStartingItem;
+    public bool canBePickedUp;
+    public bool canBeDropped;
+    public bool canBeEaten;
+    public bool canBeCarried;
+}
