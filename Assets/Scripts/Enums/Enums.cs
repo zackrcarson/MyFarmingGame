9b912b639306645976d10351772817773365a561
zackcarson <zack.carson@virginia.edu>
2020-08-23 23:24:31 -0600

    Added inventory manager functionality and item pickup script for when the player walks over it

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -1,4 +1,12 @@
-﻿public enum ToolEffect 
+﻿// Count simply returns the integer that is equal to the length of actual items in the enum, because it's the last item in the enum (wchich starts counting at 0)
+public enum InventoryLocation
+{
+    player,
+    chest,
+    count
+}
+
+public enum ToolEffect 
 {
     none,
     watering
--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -6,17 +6,47 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
     // Create a dictionary for inventory, with itemCode : itemDetails. This will be fast to access
     private Dictionary<int, ItemDetails> itemDetailsDictionary;
 
+    // InventoryItem is a struct that stores the item code, and the number held
+    public List<InventoryItem>[] inventoryLists;
+
+    // The index of the array is the inventory list location (from the InventoryLocation enum), and the value is the capacity of that inventory list
+    [HideInInspector] public int[] inventoryListCapacityIntArray;
+
     [SerializeField] private SO_ItemList itemList = null;
 
+
     // Awake will run before start! This way this class and Item don't have to fight over accessing this dictionary. If this goes in Start() instead, 
-    // We get a null reference exception
+    // we would get a null reference exception
     protected override void Awake()
     {
         base.Awake();
 
+        // Create inventory lists
+        CreateInventoryLists();
+
         // Create item details dictionary
         CreateItemDetailsDictionary();
     }
+
+
+    private void CreateInventoryLists()
+    {   
+        // Array of inventory lists of length (number of inventory locations - equal to the enum.count). Currently only 2: player and chest
+        inventoryLists = new List<InventoryItem>[(int)InventoryLocation.count];
+
+        // Create an inventory list of at each index (player and count)
+        for (int i = 0; i < (int)InventoryLocation.count; i++)
+        {
+            inventoryLists[i] = new List<InventoryItem>();
+        }
+
+        // Initialize inventory list capacity array
+        inventoryListCapacityIntArray = new int[(int)InventoryLocation.count];
+
+        // Initialize player inventory list capacity (each index is the capacity of that inventory location: player, chest)
+        inventoryListCapacityIntArray[(int)InventoryLocation.player] = Settings.playerInitialInventoryCapacity;
+    }
+
 
     /// <summary>
     /// Populates the itemDetailsDictionary from the scriptable object items list
@@ -32,6 +62,105 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
             itemDetailsDictionary.Add(itemDetails.itemCode, itemDetails);
         }
     }
+
+
+    /// <summary>
+    /// Add an item to the inventory list for that inventory location, and then destroy the gameObject! This is an overloaded method!
+    /// </summary>
+    public void AddItem(InventoryLocation inventoryLocation, Item item, GameObject gameObjectToDelete)
+    {
+        AddItem(inventoryLocation, item);
+
+        Destroy(gameObjectToDelete);
+    }
+
+
+    /// <summary>
+    /// Add an item to the inventory list for that inventory location. This is an overloaded method!
+    /// </summary>
+    public void AddItem(InventoryLocation inventoryLocation, Item item)
+    {
+        int itemCode = item.ItemCode;
+        List<InventoryItem> inventoryList = inventoryLists[(int)inventoryLocation];
+
+        // Check if the inventory already contains the item (returns position of the item, or -1 if it doesn't exist)
+        int itemPosition = FindItemInInventory(inventoryLocation, itemCode);
+
+        // If it is in the inventory, add another one to the same position. If not, create a brand new one 
+        if (itemPosition != -1)
+        {
+            AddItemAtPosition(inventoryList, itemCode, itemPosition);
+        }
+        else
+        {
+            AddItemAtPosition(inventoryList, itemCode);
+        }
+
+        // Send event that inventory has been updated
+        EventHandler.CallInventoryUpdatedEvent(inventoryLocation, inventoryLists[(int)inventoryLocation]);
+    }
+
+
+    /// <summary>
+    /// Add item to the end of the inventory list. This is an overloaded method!
+    /// </summary>
+    public void AddItemAtPosition(List<InventoryItem> inventoryList, int itemCode)
+    {
+        //InventoryItem type is a struct that stores the itemCode, and the quantity held
+        InventoryItem inventoryItem = new InventoryItem();
+        
+        // Fill the struct with the item code, and set it to 1 held, then add the struct to the end of the inventory list
+        inventoryItem.itemCode = itemCode;
+        inventoryItem.itemQuantity = 1;
+        inventoryList.Add(inventoryItem);
+
+        // Print out the entire inventory
+        DebugPrintInventoryList(inventoryList);
+    }
+
+
+    /// <summary>
+    /// Add item to a specific position in the inventory list. This is an overloaded method!
+    /// </summary>
+    public void AddItemAtPosition(List<InventoryItem> inventoryList, int itemCode, int position)
+    {
+        //InventoryItem type is a struct that stores the itemCode, and the quantity held
+        InventoryItem inventoryItem = new InventoryItem();
+        
+        // Add 1 to the existing quantity of this item code at the given index
+        int quantity = inventoryList[position].itemQuantity + 1;
+
+        // Fill the struct with the item code, and the new quantity, then add the struct to the correct location in the inventory list
+        inventoryItem.itemCode = itemCode;
+        inventoryItem.itemQuantity = quantity;
+        inventoryList[position] = inventoryItem;
+
+        // Print out the entire inventory
+        DebugPrintInventoryList(inventoryList);
+    }
+
+
+    /// <summary>
+    /// Find if an itemCode is already in the inventory.static Returns the item position
+    /// in the inventory list, or -1 if the item is not in the inventory
+    /// </summary>
+    public int FindItemInInventory(InventoryLocation inventoryLocation, int itemCode)
+    {
+        // Inventory list is a list of inventory lists, 0 is the players inventory, 1 is the chest
+        List<InventoryItem> inventoryList = inventoryLists[(int)inventoryLocation];
+
+        // Loop through the inventory list to see if the item code is equal to any of them
+        for (int i = 0; i < inventoryList.Count; i++)
+        {
+            if (inventoryList[i].itemCode == itemCode)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 
     /// <summary>
     /// Returns the itemDetails from the SO_ItemList for the itemCode, or null if the item code doesn't exist
@@ -49,5 +178,16 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
         {
             return null;
         }
+    }
+
+    private void DebugPrintInventoryList(List<InventoryItem> inventoryList)
+    {
+        // Loop through the passed in inventoryList, and print out the item details from GetItemDetails method, with the inventoryItem struct item code, 
+        // and print out the quantity in the struct 
+        foreach (InventoryItem inventoryItem in inventoryList)
+        {
+            Debug.Log("Item Description: " + InventoryManager.Instance.GetItemDetails(inventoryItem.itemCode).itemDescription + "    Item Quantity: " + inventoryItem.itemQuantity);
+        }
+        Debug.Log("**********************************************************************************");
     }
 }
