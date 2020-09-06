01bd2384303418e921aa0829ef6612be9142df4b
zackrcarson <zackrcarson@gmail.com>
2020-09-06 17:32:38 -0600

    Created core functionality for saving game states

---
+++ Assets/Scripts/SaveSystem/SceneItem.cs
@@ -0,0 +1,13 @@
+﻿[System.Serializable]
+public class SceneItem
+{
+    public int itemCode;
+    public Vector3Serializable position;
+    public string itemName;
+
+    // We will have a SceneItem Instance for every item in our scene, which will allow us to save a serializable itemCode, position, and itemName
+    public SceneItem()
+    {
+        position = new Vector3Serializable();
+    }
+}
